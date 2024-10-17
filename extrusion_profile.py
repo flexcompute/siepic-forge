7987dca5eee991427b02423320c4fe1834a3a073
Lucas Heitzmann Gabrielli <lucas@flexcompute.com>
2024-10-17 07:40:43 -0300

    Fix extrusion example for released version of photonforge
    Signed-off-by: Lucas Heitzmann Gabrielli <lucas@flexcompute.com>

--- extrusion_profile.py
+++ extrusion_profile.py
@@ -2,21 +2,19 @@ import photonforge as pf
 import siepic_forge as siepic
 from matplotlib import pyplot
 
-pf.config.default_technology = siepic.ebeam(
-)
+pf.config.default_technology = siepic.ebeam()
 
 c = pf.Component("Extrusion test")
 c.add(
-    pf.Rectangle((0, -1), (2, 1), layer="Si"),
-    
-    pf.Rectangle((2.5, -1), (3.5, 1), layer="SiN"),
-    
-    pf.Rectangle((4, -1), (7, 1), layer="Si slab"),
-    pf.Rectangle((5, -1), (6, 1), layer="Si"),
+    "Si",
+    pf.Rectangle((0, -1), (2, 1)),
+    "SiN",
+    pf.Rectangle((2.5, -1), (3.5, 1)),
+    "Si slab",
+    pf.Rectangle((4, -1), (7, 1)),
+    "Si",
+    pf.Rectangle((5, -1), (6, 1)),
 )
 
-model = pf.Tidy3DModel()
-sim = model.get_base_simulation(c, pf.C_0 / 1.55)
-sim.plot_structures(y=0)
-
+pf.tidy3d_plot(c, y=0)
 pyplot.show()
